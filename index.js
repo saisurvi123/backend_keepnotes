9fcb41c82ad5cbf7892f323c20e9e6f76eeaf6d9
saikiran survi <survisaikiran7353@gmail.com>
2022-12-17 14:03:47 +0530

    vercel deleted

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ connectToMongo();
 const app = express();
 // const host='0.0.0.0';
 app.use(cors());
-const port = 5000;
+// const port = 5000;
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
